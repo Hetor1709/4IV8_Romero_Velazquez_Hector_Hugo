c765d915575cc26eadb1e3e1860a51c08fc6627b
Hetor1709 <hromerov2400@alumno.ipn.mx>
2026-04-20 19:15:32 -0600

    chamba jimmy

--- 02FormularioHTML/Js/validarformulario.js
+++ 02FormularioHTML/Js/validarformulario.js
@@ -22,4 +22,38 @@ function validar(formulario){
           break;
       }
     }
+    if(!allValid){
+        alert("El campo nombre solo acepta letras");
+        formulario.nombre.focus();
+        return false;
+    }
+
+ var abcOK="1234567890";
+
+    var chechString= formulario.edad.value;
+
+    var allValid=true;
+    for(var i=0; i<checkString.length;i++){
+        var caracteres=checkString.charAt(i);
+      for(var j=0; j<abcOK.length;j++){
+          if(caracteres==abcOK.charAt(j)){
+              break;
+          }
+      }
+      if(j==abcOK.length){
+          allValid=false;
+          break;
+      }
+    }
+    if(!allValid){
+        alert("El campo edad solo acepta números");
+        formulario.edad.focus();
+        return false;
+    }
+    var correoelectronico= /^[^@\s]+[^@\.\s]+(\.[^@\.\s]+)$/;
+    var txt = formulario.email.value;
+    alert("Email " + (correoelectronico.test(txt)?"":"no ") + "válido");
+
+
+
 }
